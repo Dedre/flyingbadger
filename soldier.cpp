5a7bb6f53f1764daf812b261340fd8c1e78c1877
Dedre <dedreolwage@gmail.com>
2015-10-07 09:26:36 +0200

    Update soldier.cpp

--- soldier.cpp
+++ soldier.cpp
@@ -4,14 +4,19 @@ Soldier::Soldier()
 {
 	Unit::setDamage(8);
 	Unit::setHealth(100);
+	Unit::setDescription("_('x')_");
 }
 
-Unit* Soldier::clone(Unit& aUnit)
+Unit* Soldier::clone(/*Unit& aUnit*/)
 {
-	Unit* temp;
+	/*Unit* temp;
 	temp = new Soldier();
 	Unit::setHealth(aUnit.getHealth());
-	return temp;
+	return temp;*/
+	
+	//instead of above do this
+	
+	return new Soldier(*this);
 }
 
 void Soldier::attack(Unit& aUnit)
