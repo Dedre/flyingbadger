30bf3e2e844b9846bcf7b0e2ba324352563f60ed
Dedre <dedreolwage@gmail.com>
2015-10-07 09:30:43 +0200

    Update unit.h

--- unit.h
+++ unit.h
@@ -1,13 +1,16 @@
 /**
  * @file   unit.h
- * @Author Craig van Heerden U15029779
- * @date   September, 2015
+ * @Author Craig van Heerden U15029779, Dedre Olwage U15015239
+ * @date   September, 2015- October, 2015
  * @brief  Pure virtual class that defines a unit
  *
  */
-
 #ifndef UNIT_H
 #define UNIT_H
+
+#include <iostream>
+
+using namespace std;
 
 class Unit
 {
@@ -18,15 +21,19 @@ public:
 	int getDamage();
 	virtual void attack(Unit&)=0;
 	virtual void takeDamage(int)=0;
-	virtual Unit* clone(Unit&)=0;
+	virtual Unit* clone(/*Unit&*/)=0;
+	string getDescription();
 
 protected:
 	void setHealth(int pHealth);
 	void setDamage(int pDamage);
+	void setDescription(string pDescription);
+	//string getDescription();
 
 private:
 	int damage;
 	int health;
+	string description;
 	
 };
 
