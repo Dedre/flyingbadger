724e114b410b151cb9713b4dfc9e6c87f3b4cbb6
Dedre <dedreolwage@gmail.com>
2015-10-07 09:14:40 +0200

    Update elemental.cpp

--- elemental.cpp
+++ elemental.cpp
@@ -4,14 +4,20 @@ Elemental::Elemental()
 {
 	Unit::setDamage(4);
 	Unit::setHealth(85);
+	Unit::setDescription("**|` `|**");
 }
 
-Unit* Elemental::clone(Unit& aUnit)
+Unit* Elemental::clone(/*Unit& aUnit*/)
 {
-	Unit* temp;
+	/*Unit* temp;
 	temp = new Elemental();
 	Unit::setHealth(aUnit.getHealth());
-	return temp;
+	return temp;*/
+	
+	//in stead of above, just clone this
+	
+	return new Elemental (*this);
+	
 }
 
 void Elemental::attack(Unit& aUnit)
